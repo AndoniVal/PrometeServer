f5a18c9f38c82ad5bad4836520f05c031d0679db
itari-1502 <isanzr@a.mikeldi.com>
2026-05-05 13:30:30 +0200

    gffdxcvgjhbkh

--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,22 @@
-﻿
-<?php
-
-
-
+﻿<?php
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
-
 use App\Http\Controllers\PageController;
-
 use App\Http\Controllers\EconomatoController;
-
 use App\Http\Controllers\ProductoController;
 
+Route::get("/", [PageController::class, "index"]);
+Route::get("/login", [AuthController::class, "showLogin"])->name("login");
+Route::post("/login", [AuthController::class, "login"])->name("login.post");
 
+Route::middleware("auth")->group(function () {
+    Route::post("/logout", [AuthController::class, "logout"])->name("logout");
+    Route::get("/dashboard", [AuthController::class, "dashboard"])->name("dashboard");
 
-// Públicas
+    Route::get("/economato", [EconomatoController::class, "index"])->name("economato");
+    Route::post("/economato/comprar", [EconomatoController::class, "comprar"])->name("economato.comprar");
 
-Route::get('/', [PageController::class, 'index']);
-
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-
-Route::post('/login', [AuthController::class, 'login'])->name('login.post');
-
-
-
-// Protegidas
-
-Route::middleware('auth')->group(function () {
-
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-    Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
-
-
-
-    // Economato
-
-    Route::get('/economato', [EconomatoController::class, 'index'])->name('economato');
-
-    Route::post('/economato/comprar', [EconomatoController::class, 'comprar'])->name('economato.comprar');
-
-
-
-    // Productos (solo admin)
-
-    Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
-
-    Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
-
-    Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
-
+    Route::post("/productos", [ProductoController::class, "store"])->name("productos.store");
+    Route::put("/productos/{id}", [ProductoController::class, "update"])->name("productos.update");
+    Route::delete("/productos/{id}", [ProductoController::class, "destroy"])->name("productos.destroy");
 });
-
