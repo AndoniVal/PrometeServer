14639a5c941917687691f7f0028c43084d9e1570
AndoniVal <avalverdep@a.mikeldi.com>
2026-05-16 15:59:10 +0200

    agregar logica de material e intercambios

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Material extends Model
 {
     protected $table = 'materiales';
-    protected $fillable = ['id_us', 'nombre', 'tipo', 'estado'];
+    protected $fillable = ['id_us', 'id_prestado', 'nombre', 'descripcion', 'tipo', 'estado', 'imagen'];
 
     public function usuario()
     {
         return $this->belongsTo(User::class, 'id_us');
+    }
+
+    public function usuarioPrestado()
+    {
+        return $this->belongsTo(User::class, 'id_prestado');
     }
 
     public function prestamos()
